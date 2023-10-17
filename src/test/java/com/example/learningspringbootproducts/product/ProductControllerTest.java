b1991dae5a417abdd895fd7a9db5c32e3d75e83d
Enrico Schulz <107748954+SchulzEnrico@users.noreply.github.com>
2023-10-17 16:39:03 +0200

    add Test = getAllProducts

---
+++ src/test/java/com/example/learningspringbootproducts/product/ProductControllerTest.java
@@ -0,0 +1,74 @@
+package com.example.learningspringbootproducts.product;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProductControllerTest {
+
+    private final static String BASE_URI = "/api/products";
+    @Autowired
+    private MockMvc mockMvc; // Um HTTP-Requests zu simulieren
+    @Autowired
+    private ObjectMapper objectMapper; // Wandelt JSON in Java Objekte um und umgekehrt
+
+    @Test
+    @DirtiesContext
+    void getAllProducts_whenEntriesInDb_expectStatus200AndReturnEntriesAsJson() throws Exception {
+        NewProduct product = new NewProduct("TestProduct", 0.99);
+        String productAsJson = objectMapper.writeValueAsString(product);
+
+        MvcResult result = mockMvc
+                            .perform(post(BASE_URI)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(productAsJson)
+                )
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Product savedProduct = objectMapper.readValue(result.getResponse().getContentAsString(), Product.class);
+
+        List<Product> products = List.of(savedProduct);
+        String productsAsJson = objectMapper.writeValueAsString(products);
+
+        mockMvc.perform(get(BASE_URI))
+                .andExpect(status().isOk())
+                .andExpect(content().json(productsAsJson));
+    }
+
+    @Test
+    void findProductById() {
+    }
+
+    @Test
+    void getAllProductsBynPrice() {
+    }
+
+    @Test
+    void addProduct() {
+    }
+
+    @Test
+    void removeProductById() {
+    }
+
+    @Test
+    void updateProductById() {
+    }
+}
